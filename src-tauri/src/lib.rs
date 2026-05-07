6ff9291ecf299095c3a73c913d579c103fe9058c
AMIT KUMAR <sharmashyama1988@gmail.com>
2026-05-08 01:06:00 +0530

    fix: add lib.rs for Android, fix Cargo.toml lib target, use ImageMagick for ICO on Windows

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    titan_compress_lib::run();
+pub fn run() {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
